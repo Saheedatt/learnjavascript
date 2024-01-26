1eabc2bd780c275b909c3e070176095cd9418545
saheedatt <afolabisaheedat1@gmail.com>
2024-01-26 19:36:08 +0100

    Exercises

---
+++ exercises.js
@@ -0,0 +1,9 @@
+//1. Write a ternary operator to check if a given number is even or odd and log the result to the console.
+
+//2. Given a student's score, use a ternary operator to assign a grade. For example, if the score is greater than or equal to 90, assign 'A', else 'B'.
+
+//3. Write an if-else statement to check if the temperature is above 36 degrees Celsius. If it is, log "You might need to see a doctor" to the console; otherwise, log "You are okay."
+
+//4. Create a switch statement to log a message based on the day of the week. For example, if the day is Monday, log "It's the start of the week."
+
+//5. Given a person's age, use a combination of if-else and switch statements to categorize them into groups like "Child," "Teenager," "Adult," or "Senior."
