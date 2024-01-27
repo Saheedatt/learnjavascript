cf48eb56e7e284f9ac0178e86102b9d4c01b2f6c
saheedatt <afolabisaheedat1@gmail.com>
2024-01-27 09:49:38 +0100

    Day 3: Functions

---
+++ functions.js
@@ -0,0 +1,18 @@
+//function expression
+const addFunction = function add(num1, num2){
+
+}
+
+//function declaration
+function addFunction2(num1, num2){
+
+}
+
+//arrow function
+const addArrow = (num1, num2) => {
+
+}
+
+//modified arrow function
+const addArrow2 =(num1, num2) => num1 + num2
+
